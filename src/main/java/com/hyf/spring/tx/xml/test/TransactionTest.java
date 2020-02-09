b7034029f30bde04c0a2fbf1f7eaae089f761957
hyfsy <1577975140@qq.com>
2020-02-09 23:50:29 +0800

    增加编程式事务测试

--- src/main/java/com/hyf/spring/tx/xml/test/TransactionTest.java
+++ src/main/java/com/hyf/spring/tx/xml/test/TransactionTest.java
@@ -1,5 +1,6 @@
 package com.hyf.spring.tx.xml.test;
 
+import com.hyf.spring.tx.xml.pojo.Account;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +8,35 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.hyf.spring.tx.xml.service.IAccountService;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:tx.xml")
 public class TransactionTest {
 
     @Autowired
-    private  IAccountService accountService;
+    private IAccountService accountService;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     @Test
     public void testTransaction() {
         accountService.transfer(4,5,10d);
+    }
+
+    @Test
+    public void testTransactionTemplate() {
+        transactionTemplate.execute(new TransactionCallback<Account>() {
+            @Override
+            public Account doInTransaction(TransactionStatus status) {
+
+                // ... 在事务模板中写入处理逻辑
+
+                return null;
+            }
+        });
     }
 }
